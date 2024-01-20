494cfb32ee505eff022c99a9fe878344d649d5ac
joychandrauday <joychandraud@gmail.com>
2024-01-21 00:44:40 +0600

    added all the sections

---
+++ js/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+{/* <div class="slide flex">
+                        <!-- <div class="slide-text">
+                            <h1 class="text-6xl">Ride-on R15 V4 with Smile</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum voluptatum tenetur rerum rem necessitatibus cum non fugit vitae?</p>
+                            <button class="btn bg-main-btn capitalize">purchase</button>
+                        </div> -->
+                        <div class="slide-img">
+                            <img src="images/slider/bike1.png" class="mw-full" />
+                        </div>
+                    </div> */}
